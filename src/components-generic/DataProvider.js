dff37ccc3b9ed96b56e20267dfff2ac20286e292
W.A. In 't Velt <wintvelt@WAs-MacBook-Pro.fritz.box>
2020-05-07 15:18:06 +0200

    extract sampledata

--- src/components-generic/DataProvider.js
+++ src/components-generic/DataProvider.js
@@ -1,64 +1,11 @@
 // HOC which fetches data and renders loading state
 import React, { createContext, useContext, useState, useEffect } from 'react';
+import { getDataFrom } from '../sampledata';
 
 export const DataContext = createContext({
     data: {},
     setDataByKey: () => { }
 });
-
-const sampleData = {
-    'myUrl/groups': [
-        {
-            id: 'group1',
-            title: 'Foto\'s van Blob',
-            subtitle: 'Laag naar de top sinds 1985',
-            image: '/cover_2.jpg',
-            userIsAdmin: true,
-        },
-        {
-            id: 'group2',
-            title: 'In \'t Velt familiealbums',
-            subtitle: 'Door de jaren heen in Soest en daarbuiten',
-            image: '/cover_2.jpg',
-            userIsAdmin: true,
-        },
-        {
-            id: 'group3',
-            title: 'LamInt photos',
-            subtitle: 'Sharing family life',
-            image: '/cover_2.jpg',
-            userIsAdmin: false,
-        },
-    ],
-    'myUrl/groups/group2': {
-        title: 'Foto\'s van Blob',
-        subtitle: 'Laag naar de top sinds 1985',
-        stats: [
-            'since 8 Jan 2019',
-            '6 albums',
-            '492 photos',
-            '19 members'
-        ],
-        image: '/cover_2.jpg',
-        userIsAdmin: true,
-    },
-    'myUrl/groups/group2/members': [
-        { name: 'LJ van Berkestijn', avatar: '' },
-        { name: 'Paul Botje', avatar: '' },
-        { name: 'Dave del Canho', avatar: '' },
-        { name: 'Wouter In \'t Velt', avatar: '/img/me.jpg' },
-        { name: 'Fred Kleiterp', avatar: '' },
-        { name: 'Michiel Ebeling', avatar: '' },
-    ]
-}
-
-function timeout(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-const getDataFrom = async (source) => {
-    await timeout(3000);
-    return sampleData[source] || source;
-}
 
 export const useApiData = (key, source) => {
     const { data, setDataByKey } = useContext(DataContext);

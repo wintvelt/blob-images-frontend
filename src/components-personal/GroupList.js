cc90fce75be0e238c6aead49cc717516b1179a4e
W.A. In 't Velt <wintvelt@WAs-MacBook-Pro.fritz.box>
2020-05-26 20:07:44 +0200

    add redirect to new group when no groups

--- src/components-personal/GroupList.js
+++ src/components-personal/GroupList.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 import { useApiData } from '../../src/components-generic/DataProvider';
 import GroupCardLayout from './GroupCardLayout';
@@ -6,15 +7,20 @@ import GroupCardAdd from './GroupCardAdd';
 import CardList from '../components-generic/CardList';
 
 const GroupList = () => {
-    const source = '/groups';
-    const groups = useApiData('groups', source);
+    const router = useRouter();
+    const groups = useApiData('groups', '/groups');
     const groupsList = groups.data || [1, 2].map(id => ({ id, isLoading: true }));
+    useEffect(() => {
+        if (groupsList.length === 0) {
+            router.push('/personal/groups/[id]/edit', '/personal/groups/new/edit')
+        }
+    }, [groupsList]);
     const groupsWithEdit = (groups.isLoading) ?
         groupsList
         : groupsList.map(item => ({ ...item.group, withEdit: true }));
     return <div style={{ padding: '24px' }}>
         <CardList list={groupsWithEdit} component={GroupCardLayout} addComponent={GroupCardAdd}
-            width={3} spacing={2} isLoading={groups.isLoading}/>
+            width={3} spacing={2} isLoading={groups.isLoading} />
     </div>
 }
 

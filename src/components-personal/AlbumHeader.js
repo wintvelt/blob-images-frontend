9140454509c6cef607c0685550ffef3811ff7eca
W.A. In 't Velt <wintvelt@WAs-MacBook-Pro.fritz.box>
2020-06-29 13:08:55 +0200

    try selectorFamily

--- src/components-personal/AlbumHeader.js
+++ src/components-personal/AlbumHeader.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import { useRouter } from 'next/router';
+import { useRecoilValue } from 'recoil';
+import { selectorFamily } from 'recoilUtils';
+import { API } from 'aws-amplify';
 
-import { useApiDataValue } from '../../src/data/apiData';
 import AlbumHeaderLayout from './AlbumHeaderLayout';
+
+const albumQuery = selectorFamily({
+    key: 'Album',
+    get: (groupId, albumId) => async () => {
+        const source = `/groups/${groupId}/albums/${albumId}`;
+        const response = await API.get('blob-images', source);
+        if (response.error) {
+            throw response.error;
+        }
+        return response;
+    },
+});
 
 const AlbumHeader = () => {
     const router = useRouter();
     const groupId = router.query && router.query.id;
     const albumId = router.query && router.query.albumid;
-    const source = `/groups/${groupId}/albums/${albumId}`;
-    const albumData = useApiDataValue('album',source);
-    return <AlbumHeaderLayout album={albumData}/>
+    const albumData = useRecoilValue(albumQuery(groupId, albumId));
+    return <AlbumHeaderLayout album={albumData} />
 }
 
 export default AlbumHeader;

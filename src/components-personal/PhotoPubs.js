5ea46b10abe4895a930acce7e446aa596280b632
W.A. In 't Velt <wintvelt@WAs-MacBook-Pro.fritz.box>
2020-08-24 18:15:23 +0200

    add link to albums in publications

--- src/components-personal/PhotoPubs.js
+++ src/components-personal/PhotoPubs.js
@@ -8,6 +8,7 @@ import { useRecoilValueLoadable } from 'recoil';
 import { userAlbums } from '../data/userData';
 import { publicationState } from '../data/activeTree-Photo';
 import { makeImageUrl } from '../components-generic/imageProvider';
+import { useSetLoadingPath } from '../data/loadingData';
 
 const useStyles = makeStyles(theme => ({
     list: {
@@ -30,20 +31,28 @@ const useStyles = makeStyles(theme => ({
 
 const PhotoPubs = ({ photo }) => {
     const classes = useStyles();
+    const setLoadingPath = useSetLoadingPath();
     const albumsData = useRecoilValueLoadable(userAlbums);
     const albums = (albumsData.state === 'hasValue')? albumsData.contents : [];
+    console.log({albums});
     const photoId = photo?.PK?.slice(2);
     const source = `/publications/${photoId}`;
     const pubData = useRecoilValueLoadable(publicationState(source));
     const pubs = (pubData.state === 'hasValue')? pubData.contents : [];
     const pubAlbumIds = pubs.map(pub => pub.PK.split('#')[1]); 
+
     return <List dense className={classes.list} subheader={
         <ListSubheader className={classes.noPadList}>Publicaties in albums van mijn groepen</ListSubheader>
     }>
         {albums.map(album => {
             const { id, name, groupName, image } = album;
+            const groupId = album.PK.slice(2);
+            const albumUrl = `/personal/groups/${groupId}/albums/${id}`;
+            const onClick = () => {
+                setLoadingPath('/personal/groups/[id]/albums/[albumid]', albumUrl);
+            }
             const imageUrl = makeImageUrl(image?.url, 40);
-            return <ListItem key={id} className={classes.noPadList}>
+            return <ListItem button key={id} className={classes.noPadList} onClick={onClick}>
                 <ListItemIcon><img src={imageUrl} className={classes.albumImg} /></ListItemIcon>
                 <ListItemText primary={name} secondary={groupName} />
                 <ListItemSecondaryAction className={classes.actions}>

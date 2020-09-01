6da6eb0e7030cf4f5e297f31efe088bbd7110832
W.A. In 't Velt <wintvelt@WAs-MacBook-Pro.fritz.box>
2020-09-01 19:03:33 +0200

    translate benefits

--- src/components-home/Benefits.js
+++ src/components-home/Benefits.js
@@ -4,28 +4,29 @@ import FeatureBlock from './FeatureBlock';
 const featureList = [
     {
         icon: 'verified_user',
-        title: 'Safe',
+        title: 'Privé',
         descriptions: [
-            'Your photos will always be visible only to you and the ones you trust. ' +
-            ' Share them with your friends, your family, ' +
-            'or with your team. Safely and securily stored in a trusted environment.'
+            "Je foto's zijn altijd uitsluitend zichtbaar voor jou, en voor degenen die je vertrouwt. " +
+            ' Deel ze met je vrienden, je familie, of met je club. ' +
+            'Veilig en beschermd opgeslagen op een omgeving binnen de EU.'
         ]
     },
     {
         icon: 'touch_app',
-        title: 'Simple',
+        title: 'Simpel',
         descriptions: [
-            'Just photos. No ads, no spam, clutter. Simple drag and drop. Easy upload and download. ' +
-            'Straightforward sharing. The basics you need, and nothing else.'
+            "Alleen foto's van jouw club. Geen reclame, geen spam. Simpel te bedienen. " +
+            'Uploads en downloads zijn rechttoe rechtaan. ' +
+            'Makkelijk te delen met jouw club. Gewoon de basis die je nodig hebt, verder niks.'
         ]
     },
     {
         icon: 'person_pin',
-        title: 'Personal',
+        title: 'Persoonlijk',
         descriptions: [
-            'Make it your own. Add albums, titles, and pick favorites. ' +
-            'The people you invite get a personal message, to join just your group. ' +
-            'No need for everyone to be on whatever social platform.'
+            "Voeg albums en titels toe, en kies je favoriete foto's. "  +
+            'De clubgenoten die je uitnodigt krijgen een persoonlijke mail, en kunnen zo ook lid worden. ' +
+            'Geen vreemden van buiten, en je hoeft geen lid te zijn van welk vaag social media platform dan ook.'
         ]
     },
 ]

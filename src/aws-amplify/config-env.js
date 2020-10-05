0489b42be34e30bbd6f38581a18706a6896cdc14
W.A. In 't Velt <wintvelt@WAs-MacBook-Pro.fritz.box>
2020-10-05 15:18:44 +0200

    rename env to next_public

--- src/aws-amplify/config-env.js
+++ src/aws-amplify/config-env.js
@@ -1,6 +1,6 @@
-const branch = process.env.BRANCH || 'development';
+const branch = process.env.NEXT_PUBLIC_BRANCH || 'development';
 const isMaster = (branch === 'master');
-console.log(process.env.BRANCH, process.env.NEXT_PUBLIC_VERSION);
+console.log(process.env.NEXT_PUBLIC_BRANCH, process.env.NEXT_PUBLIC_VERSION);
 
 const makeSwitch = (devValue, defaultValue) => (
     (!isMaster) ? devValue : defaultValue

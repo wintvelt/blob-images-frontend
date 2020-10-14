456492c8a43eaa93538991fbd71fcb2af75fb523
W.A. In 't Velt <wintvelt@WAs-MacBook-Pro.fritz.box>
2020-10-14 15:22:05 +0200

    fix metadata

--- pages/personal/groups/[id]/albums/[albumid].js
+++ pages/personal/groups/[id]/albums/[albumid].js
@@ -18,7 +18,7 @@ const AlbumMain = () => {
     const groupId = activeAlbum.groupId;
     const albumId = activeAlbum.albumId;
     const userIsAdmin = activeAlbum.userIsAdmin;
-    const photoMetaData = { action: 'albumphoto', groupId, albumId };
+    const photoMetaData = { action: 'albumphoto', groupid: groupId, albumid: albumId };
     const pond = useRef();
     const reloadPhotos = useReloadAlbumPhotoIds();
     const reloadAlbum = useReloadActiveAlbum();

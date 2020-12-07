db941273765a195b3f430a1fffc336a0e975691a
Wouter <wintvelt@me.com>
2020-12-07 15:19:07 +0100

    add Google tag manager

--- pages/_document.js
+++ pages/_document.js
@@ -15,7 +15,19 @@ const GTAG = process.env.GOOGLE_TAG;
 export default class MyDocument extends Document {
     render() {
         return (<Html xmlns="http://www.w3.org/1999/xhtml" lang="en" >
-            <Head > { /* PWA primary color */}
+            <Head >
+                { /* Google tag manager */}
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-M6L83BM');`,
+                    }}
+                />
+                { /* PWA primary color */}
                 <meta name="theme-color"
                     content={theme.palette.primary.main} />
                 <link rel="stylesheet"
@@ -41,6 +53,9 @@ console.log('did run google script');
             <body
                 style={bodyStyle}
             >
+                <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M6L83BM"
+                    height="0" width="0" style="display:none;visibility:hidden"></iframe>
+                </noscript>
                 <Main />
                 <NextScript />
             </body>
